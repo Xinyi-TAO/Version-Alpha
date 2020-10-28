45a6338ff0e6bb889140754d465b9973ee5628c0
Xinyi-TAO <72512869+Xinyi-TAO@users.noreply.github.com>
2020-10-28 23:40:24 +0100

    GameOverManu + MainMenu

---
+++ Version Alpha/Assets/Script/GameOverManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject gameOverUI;
+
+    public static GameOverManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de GameOverManager dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerFailed()
+    {
+        gameOverUI.SetActive(true);
+    }
+    public void RetryButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        gameOverUI.SetActive(false);
+    }
+
+    public void BackButton()
+    {
+        gameOverUI.SetActive(false);
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Version Alpha/Assets/Script/MainMenuContoler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuContoler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel_1;
+
+    public GameObject Panel_2;
+    void Start()
+    {
+        Panel_2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onClickSelectLevel()
+    {
+        Panel_2.SetActive(true);
+        Panel_1.SetActive(false);
+    }
+
+    public void onClickBack()
+    {
+        Panel_2.SetActive(false);
+        Panel_1.SetActive(true);
+    }
+
+    public void onClickScene1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+}
